2296730357a4312cfcfbba9ab75e252db8d74485
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-01-31 11:16:40 +0100

    Initial commit

---
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -0,0 +1,109 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Launcher;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.util.FormValidation;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+
+public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
+
+    private final String name;
+    private boolean useFrench;
+
+    @DataBoundConstructor
+    public HelloWorldBuilder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUseFrench() {
+        return useFrench;
+    }
+
+    @DataBoundSetter
+    public void setUseFrench(boolean useFrench) {
+        this.useFrench = useFrench;
+    }
+    
+    @Override
+   public BuildStepMonitor getRequiredMonitorService() {
+      return BuildStepMonitor.BUILD;
+   }
+
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+       listener.getLogger().println("This is the start " + getDate());
+        
+        Thread.sleep(10000);
+        
+        listener.getLogger().println("10 Seconds passed: " + getDate());
+        
+        Thread.sleep(10000);
+        
+        listener.getLogger().println("10 Seconds passed (2): " + getDate());
+        
+        if (useFrench) {
+            listener.getLogger().println("Bonjour, " + name + "!");
+        } else {
+            listener.getLogger().println("Hello, " + name + "!");
+        }
+    }
+    
+    public String getDate() {
+       SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//dd/MM/yyyy
+       Date now = new Date();
+       String strDate = sdfDate.format(now);
+       return strDate;
+    }
+
+    @Symbol("greet")
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
+            if (value.length() < 4)
+                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_tooShort());
+            if (!useFrench && value.matches(".*[éáàç].*")) {
+                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
+            }
+            return FormValidation.ok();
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return Messages.HelloWorldBuilder_DescriptorImpl_DisplayName();
+        }
+
+    }
+
+}
